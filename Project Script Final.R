b933bd11972f09f60e72633a594d244e17111b50
kgm223 <55505928+kgm223@users.noreply.github.com>
2022-05-04 17:44:25 -0400

    Add files via upload

---
+++ Project Script Final.R
@@ -0,0 +1,17 @@
+library(here)
+library(tidyverse)
+library(readr)
+library(ggplot2)
+Combo  <- read_csv("SPY and Covidv4.csv")
+Combo$Date <- as.Date(Combo$Date)
+#Combo$Covid_Change <- as.vector(Combo$Covid_Change)
+#Combo$SPY_Change <- as.vector(Combo$SPY_Change)
+View(Combo)
+p <- ggplot(data = Combo)
+
+p + geom_line(mapping=aes(group=1, color="Covid", x=Date,y=Covid_Change)) +
+  geom_line(mapping=aes(group=1, color="SPY", x= Date, y=SPY_Change)) +
+  geom_point((mapping=aes(x= Date, y=SPY_Change))) +
+  geom_point((mapping=aes(x= Date, y=Covid_Change))) +
+  labs(title = "Monthly S&P 500 Returns vs Covid Case Increases (May 2020-March 2021)", y="Percent Change") 
+
